b249280d97d0a90d7b622b6255d3230c50ea68b0
Shatha3004 <Shatha2004@gmail.com>
2024-10-20 17:03:57 +0530

    changes

--- main.js
+++ main.js
@@ -11,17 +11,3 @@ prev.addEventListener("click",function(){
     document.querySelector('.slide').prepend(item[item.length-1])
 })
 
-
-var first=document.getElementById("first");
-var second=document.getElementById("second");
-var third=document.getElementById("third");
-
-var content1=document.getElementById("r1");
-var content2=document.getElementById("r2");
-var content3=document.getElementById("r3");
-
-
-if(first == checked)
-{
-    
-}
