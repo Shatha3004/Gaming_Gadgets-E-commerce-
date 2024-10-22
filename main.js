f5ef074ab7f49acbd93969970dd9e1b7ade7bc8b
Shatha3004 <Shatha2004@gmail.com>
2024-10-22 18:08:36 +0530

    images upload

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ prev.addEventListener("click",function(){
     document.querySelector('.slide').prepend(item[item.length-1])
 })
 
+
+var first=document.getElementById("first");
+var second=document.getElementById("second");
+var third=document.getElementById("third");
+
+var content1=document.getElementById("r1");
+var content2=document.getElementById("r2");
+var content3=document.getElementById("r3");
+
+
+if(first == checked)
+{
+    
+}
